7066d5c960f55c537c671837dd9b747eb3dab4b9
James L <hello@imjames.me>
2015-08-23 20:41:04 +0100

    Added gulp-sourcemaps.

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -9,13 +9,18 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var sass = require('gulp-ruby-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync');
 
 gulp.task('sass', function () {
   return sass('./src/styles/main.scss', {
-    style: 'compressed'
+    style: 'compressed', sourcemap: true
     })
     .pipe(rename('main.css'))
+    .pipe(sourcemaps.write('maps', {
+      includeContent: false,
+      sourceRoot: './src/styles/main.scss'
+    }))
     .pipe(gulp.dest('./release/css'))
     .pipe(browserSync.reload({stream:true}));
 });
